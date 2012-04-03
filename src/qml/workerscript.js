61f877d8ce9453d2f8ef988d8a9645be96c7b3c7
Simon Pena <spena@igalia.com>
2012-04-03 23:28:14 +0200

    Update worker thread management
    Refactor worker script so it's possible to asynchronously load content from
    the database (besides fetching remote content from the network)

--- src/qml/workerscript.js
+++ src/qml/workerscript.js
@@ -3,15 +3,25 @@ Qt.include('constants.js')
 Qt.include('storage.js')
 
 WorkerScript.onMessage = function(message) {
-            fetchAsync(message.url)
+            switch (message.action) {
+            case REMOTE_FETCH_ACTION:
+                remoteFetch(message.url)
+                break
+            case LOCAL_FETCH_ACTION:
+                localFetchManager(message.query, message.model, message.args)
+                break
+            default:
+                console.debug('Unsupported action:', message.action)
+                break
+            }
         }
 
-function fetchAsync(serviceUrl) {
+function remoteFetch(serviceUrl) {
     var xhr = new XMLHttpRequest
     xhr.onreadystatechange = function() {
                 if (xhr.readyState === XMLHttpRequest.DONE) {
                     WorkerScript.sendMessage({
-                                                 url: serviceUrl,
+                                                 action: REMOTE_FETCH_RESPONSE,
                                                  status: xhr.status,
                                                  response: xhr.responseText
                                              })
@@ -19,4 +29,36 @@ function fetchAsync(serviceUrl) {
             }
     xhr.open("GET", serviceUrl)
     xhr.send()
+}
+
+function localFetchManager(query, model, args) {
+    var elements = []
+    switch (query) {
+    case 'loadFavoriteStops':
+        elements = loadFavoriteStops(args.codes)
+        break
+    case 'loadStopsByLine':
+        elements = loadStopsByLine({ direction: args.direction, lineCode: args.lineCode })
+        break
+    case 'loadLines':
+        elements = loadLines({ direction: args.direction })
+        break
+    case 'searchStopsByCoordinate':
+        elements = searchStopsByCoordinate(args)
+        break
+    case 'searchStopsByName':
+        elements = searchStopsByName(args.name)
+        break
+    case 'loadLinesByStop':
+        elements = loadLinesByStop(args.code)
+        break
+    default:
+        console.debug('Unsupported query:', query)
+        break
+    }
+    for (var i = 0; i < elements.length; i ++) {
+        model.append(elements[i])
+    }
+    model.sync()
+    WorkerScript.sendMessage({ action: LOCAL_FETCH_RESPONSE })
 }
